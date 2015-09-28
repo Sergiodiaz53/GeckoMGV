a20318305d69a26b6a67d79aae60637ff3a37452
Sergiodiaz53 <sergiodiazdp@gmail.com>
2015-09-28 13:46:54 +0200

    CSB2CSV added

--- scripts/forms.py
+++ scripts/forms.py
@@ -26,5 +26,19 @@ class kmersForm(forms.Form):
         super(kmersForm, self).__init__(*args, **kwargs)
         print self.user
         print userFile.objects.filter(user=self.user)
-        self.fields['parameter1'] = forms.ChoiceField(choices=[(file.file.name, file.file.name)
-                                                             for file in userFile.objects.filter(user=self.user)])
+        self.fields['parameter1'] = forms.ChoiceField(choices=[(file.file.name, file.file.name) for file in userFile.objects.filter(user=self.user)], label="Filename", widget=forms.Select(attrs={'class':'selector','id': 'Filename'}))
+
+class csb2csvForm(forms.Form):
+    parameter1 = forms.ChoiceField(label="CSB file", widget=forms.Select(attrs={'class':'selector','id': 'CSBfile'}))
+    parameter2 = forms.ChoiceField(label="Master file", widget=forms.Select(attrs={'class':'selector','id': 'Masterfile'}))
+
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        self.request = kwargs.pop('request', None)
+        super(csb2csvForm, self).__init__(*args, **kwargs)
+        print self.user
+        print userFile.objects.filter(user=self.user)
+        self.fields['parameter1'] = forms.ChoiceField(choices=[(file.file.name, file.file.name) for file in userFile.objects.filter(user=self.user)], label="CSB file", widget=forms.Select(attrs={'class':'selector','id': 'CSBfile'}))
+        self.fields['parameter2'] = forms.ChoiceField(choices=[(file.file.name, file.file.name) for file in userFile.objects.filter(user=self.user)], label="Master file", widget=forms.Select(attrs={'class':'selector','id': 'Masterfile'}))
+
