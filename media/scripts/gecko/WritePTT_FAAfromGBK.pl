f31d0f06a80983f1f7cd9ade7043ee30a08fbcad
Pablo Rodriguez <pabrod@uma.es>
2018-04-05 17:24:57 +0200

    Anot Perl Scrip changes

--- media/scripts/gecko/WritePTT_FAAfromGBK.pl
+++ media/scripts/gecko/WritePTT_FAAfromGBK.pl
@@ -23,116 +23,67 @@ $org_name_spaces =~ s/\_/ /g;
 my $gene = "-";
 open(ARQ,"$gbk_file");
 while (<ARQ>)
+{
+  if (/ORGANISM\s+(.+)/)
   {
-    if (/ORGANISM\s+(.+)/)
-      {
-	$org = $1;
-      }
-    elsif (/^LOCUS\s+\S+\s+(\d+)\s+/) #LOCUS       CIAT899_chromosome   3837061 bp    DNA     linear       05-MAR-2012
-      {
-   	$genome_size = $1;
-      }
-    elsif (/CDS\s*(\d+)\.\.(\d+)/)
-      {       
-	$strand =  "f";
-	$start = $1;
-	$stop = $2;
-	$gi = "";
-	$found_gene = "true";	       
-      }
-    elsif (/CDS\s*complement\((\d+)\.\.(\d+)/)
-      {       
-	$strand =  "r";
-	$start = $1;
-	$stop = $2;
-	$gi = "";
-	$found_gene = "true";
-      }
-    elsif (/CDS\s+.*join\((\d+)\.\..+\.\.(\d+)/)
-      {
-        $strand =  "f";
-        $start = $1;
-        $stop = $2;
-        $gi = "";
-        $found_gene = "true";
-      }
-    elsif (($found_gene eq "true") && (/\/gene\=\"(.+)\"/))
-      {       
-	$gene = $1;
-	if ($gene =~ /hypothetical/)
-	  {
-	    $gene = "-";
-	  }
-      }      
-    elsif (($found_gene eq "true") && (/\s+\/product=\"(.+)\"/))
-      {
-	$product = $1;
-      }
-    elsif (($found_gene eq "true") && (/\s+\/protein_id=\"(.+)\"/))
-      {
-	#$syn = $1;
-	$found_gene = "false";
-	#$size = ($stop - $start - 2)/3; 
-	#print NEW "\t$start..$stop\t$strand\t$size\t$syn\t$gene\t$syn\t\-\t\-\t$product\n";
-	#$gene = "-";
-      }
-#    elsif (/translation=\"(\w+)\"/)    #/translation="MDFTEEFSQIYKKEKCKIDTKSQFWMYMKQFNKKLKYN"
-#      {
-#	$seq = $1;
-#	$found_seq = "false";
-#	$gi = $syn if ((not defined $gi) || ($gi eq ""));
-##	print FAA ">gi|$gi|ref|$syn| $product [$org_name_spaces]\n";
-##	print FAA "$seq\n";
-#	$size = length($seq);
-#	#print PTT "$start..$stop\t$strand\t$size\t$gi\t$gene\t$syn\t\-\t\-\t$product\n";
-#	$ptt_line .= "$start..$stop\t$strand\t$size\t$gi\t$gene\t$syn\t\-\t\-\t$product\t\n";
-#	$cont_genes++;
-#	$gene = "-";
-#      }
-#    elsif (/translation=\"(\w+)/)   
-#      {
-#	$seq = $1;
-#	$found_seq = "true";      
-#      }
-#    #elsif (($found_seq eq "true") && (/\s+([A-Z]+)/))    
-#    elsif ((/\s+([A-Z]*)\"/) && ($found_seq eq "true"))
-#      {	
-#	$seq .= $1;
-#	$found_seq = "false";
-#	$gi = $syn if ((not defined $gi) || ($gi eq ""));
-#	#antes print FAA ">gi|$gi|ref|$syn|$gene|$product [$org]\n";
-#	#antes print FAA ">gi|$gi|ref|$syn| $product [$org_name_spaces]\n";
-##	print FAA ">gi|$gi|ref|$syn| $product [$org]\n";
-##	print FAA "$seq\n";
-#        $size = length($seq);	
-#	#print PTT "$start..$stop\t$strand\t$size\t$gi\t$gene\t$syn\t\-\t\-\t$product\n";
-#	$ptt_line .= "$start\t$stop\t$strand\t$size\t$gi\t$gene\t$syn\t\-\t\-\t$product\t\n";
-#	$cont_genes++;
-#	$gene = "-";
-#      }
-#    elsif ( (/\s+([A-Z]+)/) && ($found_seq eq "true"))
-#      {
-#	$seq .= $1;	
-#      }
-    elsif (/\s+\/db_xref=\"GI\:(.+)\"/)
-      {
-	$gi = $1;
-	$syn = $gi if ((not defined $syn) || ($syn eq ""));	
-	$gi = $syn if ((not defined $gi) || ($gi eq ""));
-	$size = abs($stop-$start);
-	$ptt_line .= "$start\t$stop\t$strand\t$size\t$gi\t$gene\t$syn\t\-\t\-\t$product\t\n";
-	$cont_genes++;
-	$gene = "-";	
-	#$found_seq = "false";
-	#$gene = "-";
-      }
-    elsif (/\s+\/locus_tag=\"(.+)\"/)
-      {
-	$syn = $1;
-	#$found_seq = "false";
-	#$gene = "-";
-      }	 
+    $org = $1;
   }
+  elsif (/^LOCUS\s+\S+\s+(\d+)\s+/) #LOCUS       CIAT899_chromosome   3837061 bp    DNA     linear       05-MAR-2012
+  {
+    $genome_size = $1;
+  }
+  elsif (/CDS\s*(\d+)\.\.(\d+)/)
+  {       
+    $strand =  "f";
+    $start = $1;
+    $stop = $2;
+    $gi = "";
+    $found_gene = "true";	       
+  }
+  elsif (/CDS\s*complement\((\d+)\.\.(\d+)/)
+  {       
+    $strand =  "r";
+    $start = $1;
+    $stop = $2;
+    $gi = "";
+    $found_gene = "true";
+  }
+  elsif (/CDS\s+.*join\((\d+)\.\..+\.\.(\d+)/)
+  {
+    $strand =  "f";
+    $start = $1;
+    $stop = $2;
+    $gi = "";
+    $found_gene = "true";
+  }
+  elsif (($found_gene eq "true") && (/\/gene\=\"(.+)\"/))
+  {       
+    $gene = $1;
+    if ($gene =~ /hypothetical/)
+    {
+      $gene = "-";
+    }
+  }      
+  elsif (($found_gene eq "true") && (/\s+\/product=\"(.+)\"/))
+  {
+   $product = $1;
+  }
+  elsif (($found_gene eq "true") && (/\s+\/protein_id=\"(.+)\"/))
+  {
+    $gi = $1;
+    $syn = $gi if ((not defined $syn) || ($syn eq ""));	
+    $gi = $syn if ((not defined $gi) || ($gi eq ""));
+    $size = abs($stop-$start);
+    $ptt_line .= "$start\t$stop\t$strand\t$size\t$gi\t$gene\t$syn\t\-\t\-\t$product\t\n";
+    $cont_genes++;
+    $gene = "-";
+  }
+  elsif (/\s+\/locus_tag=\"(.+)\"/)
+  {
+    $syn = $1;
+  }	 
+}
+
 print PTT "$org, complete genome - 1..$genome_size\n";
 #print PTT "$org_name_spaces, complete genome - 1..$genome_size\n";
 print PTT "$cont_genes proteins\n";
