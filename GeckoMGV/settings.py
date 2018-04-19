924f1e5be543374e9b87a16711ebaa27f05969cc
Sergio Díaz del Pino <sergiodiazdp@gmail.com>
2018-04-19 13:33:25 +0200

    New Pistacho merge update

--- GeckoMGV/settings.py
+++ GeckoMGV/settings.py
@@ -98,11 +98,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-#STATIC_URL = '/geckomgv/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "MGV", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "MGV", "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "MGV", "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "MGV", "static"),)
 
 # Media files
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
